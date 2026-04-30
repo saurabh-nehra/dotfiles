95c307ce48ce3aa718fc0fa17e47465aa2f354ac
Saurabh <souravnehravs@gmail.com>
2026-04-30 20:07:18 +0530

    updated nvim config to dustyphilips latest

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,4 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      transparent_background = true,
-    },
-  },
+  { "catppuccin/nvim", name = "catppuccin" },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 }
--- .config/nvim/lua/plugins/extend-mini-files.lua
+++ .config/nvim/lua/plugins/extend-mini-files.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-mini/mini.files",
-  enabled = false,
   keys = {
     {
       "<leader>e",
